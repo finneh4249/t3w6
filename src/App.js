c381b1088a69537561d46a3f748fc6b40a52210b
Ethan Cornwill <ethancornwill@outlook.com>
2024-11-19 20:50:47 +1100

    Refactor: Update App component and styles for improved layout and button functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import PokemonCard from './components/PokemonCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      numberOfPokemon: 6
+    }
+  }
+
+  changePokemonNumber = action =>
+    this.setState(prevState => {
+      const { numberOfPokemon } = prevState
+      if (numberOfPokemon <= 0 && action === 'decrease') return null
+      if (numberOfPokemon >= 6 && action === 'increase') return null
+      const newNumberOfPokemon =
+        action === 'decrease' ? numberOfPokemon - 1 : numberOfPokemon + 1
+      return { numberOfPokemon: newNumberOfPokemon }
+    })
+
+  render () {
+    return (
+      <>
+        <h1>My Pokemon Team</h1>
+        <h5>Number of Pokemon: {this.state.numberOfPokemon}</h5>
+        <div className='pokemonButtons'>
+          <button onClick={() => this.changePokemonNumber('increase')}>
+            Add
+          </button>
+          <button onClick={() => this.changePokemonNumber('decrease')}>
+            Remove
+          </button>
+        </div>
+
+        <PokemonCard />
+      </>
+    )
+  }
 }
-
-export default App;
